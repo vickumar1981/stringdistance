8ee9154fa3beaec4136e8de47f05b9b6ba24a4c9
Vic Kumar <vickumar@gmail.com>
2018-02-15 02:29:55 -0500

    Add explicit return type for hamming distance.

--- src/main/scala/org/stringdistance/algo/Hamming.scala
+++ src/main/scala/org/stringdistance/algo/Hamming.scala
@@ -1,7 +1,7 @@
 package org.stringdistance.algo
 
 trait Hamming {
-  def hamming(s1: String, s2: String) = {
+  def hamming(s1: String, s2: String): Int = {
     if (s1.length != s2.length) -1
     else {
       s1.zip(s2).map { case (ch1, ch2) => if (ch1 == ch2) 0 else 1 }.sum
