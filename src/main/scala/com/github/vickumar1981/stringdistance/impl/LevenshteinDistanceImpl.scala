a49f4ad257c4ba7bcf719898af4e15c12f8287f6
Vic Kumar <vickumar@gmail.com>
2018-11-18 13:39:56 -0500

    Remove extra space.

--- src/main/scala/com/github/vickumar1981/stringdistance/impl/LevenshteinDistanceImpl.scala
+++ src/main/scala/com/github/vickumar1981/stringdistance/impl/LevenshteinDistanceImpl.scala
@@ -4,7 +4,7 @@ import scala.math.min
 
 trait LevenshteinDistanceImpl {
   protected def levenshtein(a: String, b: String): Int =
-    ((0 to b.size).toList /: a) ((prev, x) =>
+    ((0 to b.size).toList /: a)((prev, x) =>
       (prev zip prev.tail zip b).scanLeft(prev.head + 1) {
         case (h, ((d, v), y)) => min(min(h + 1, v + 1), d + (if (x == y) 0 else 1))
       }) last
