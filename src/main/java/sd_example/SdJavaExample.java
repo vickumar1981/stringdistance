8805c786c25a541ce565c2b2646e4aea66773111
Vic Kumar <vickumar@gmail.com>
2020-03-26 01:39:42 -0400

    No type inference in java example, b/c java 8.

--- src/main/java/sd_example/SdJavaExample.java
+++ src/main/java/sd_example/SdJavaExample.java
@@ -4,7 +4,7 @@ import com.github.vickumar1981.stringdistance.util.ArrayDistance;
 
 public class SdJavaExample {
     public static void main(String[] args) {
-        var levenshtein = ArrayDistance.levenshtein(
+        Double levenshtein = ArrayDistance.levenshtein(
             new Ch[]{Ch.of('m'), Ch.of('a'), Ch.of('r'), Ch.of('t'), Ch.of('h'), Ch.of('a')},
             new Ch[]{Ch.of('m'), Ch.of('a'), Ch.of('r'), Ch.of('h'), Ch.of('t'), Ch.of('a')}
         );
