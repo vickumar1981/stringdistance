0afc138d0ef7ca8e364917fc47b12e0835784a0d
João Costa <jdpc557@gmail.com>
2020-10-25 15:18:47 +0000

    Delay sound distance Array[Char] conversion

--- src/main/scala/com/github/vickumar1981/stringdistance/impl/sound/SoundexImpl.scala
+++ src/main/scala/com/github/vickumar1981/stringdistance/impl/sound/SoundexImpl.scala
@@ -4,7 +4,7 @@ import com.github.vickumar1981.stringdistance.interfaces.sound.CommonSoundAlgo._
 import com.github.vickumar1981.stringdistance.interfaces.sound.SoundexAlgo
 
 private[stringdistance] trait SoundexImpl {
-  private def compare(a: Array[Char], b: Array[Char]): Option[Boolean] =
+  private def compare(a: String, b: String): Option[Boolean] =
     if (a.length == 0 || !isAlpha(a.head) || b.length == 0 || !isAlpha(b.head)) None
     else if (a.head.toLower != b.head.toLower) Some(false)
     else SoundexAlgo.compute(a).filter(_.length > 0).flatMap { se1 =>
@@ -12,5 +12,5 @@ private[stringdistance] trait SoundexImpl {
     }
 
   protected def soundex(a: String, b: String): Boolean =
-    compare(a.toCharArray, b.toCharArray).getOrElse(false)
+    compare(a, b).getOrElse(false)
 }
