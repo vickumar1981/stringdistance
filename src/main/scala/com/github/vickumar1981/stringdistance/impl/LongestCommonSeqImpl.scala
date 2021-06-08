8a6519375136a25532c16186405f481e7b928b49
Vic Kumar <vickumar@gmail.com>
2021-06-08 16:49:52 -0400

    Update variable names for readability.

--- src/main/scala/com/github/vickumar1981/stringdistance/impl/LongestCommonSeqImpl.scala
+++ src/main/scala/com/github/vickumar1981/stringdistance/impl/LongestCommonSeqImpl.scala
@@ -2,18 +2,17 @@ package com.github.vickumar1981.stringdistance.impl
 
 private[stringdistance] trait LongestCommonSeqImpl {
   protected def longestCommonSeq[T](s1: Array[T], s2: Array[T]): Int = {
-    val m = s1.length
-    val n = s2.length
-    val T = Array.ofDim[Int](m + 1, n + 1)
-    for (i <- 1 to m) {
-      for (j <- 1 to n) {
+    val (s1Len, s2Len) = (s1.length, s2.length)
+    val dist = Array.ofDim[Int](s1Len + 1, s2Len + 1)
+    for (i <- 1 to s1Len) {
+      for (j <- 1 to s2Len) {
         if (s1(i - 1) == s2(j - 1)) {
-          T(i)(j) = T(i - 1)(j - 1) + 1
+          dist(i)(j) = dist(i - 1)(j - 1) + 1
         } else {
-          T(i)(j) = math.max(T(i - 1)(j), T(i)(j - 1))
+          dist(i)(j) = math.max(dist(i - 1)(j), dist(i)(j - 1))
         }
       }
     }
-    T(m)(n)
+    dist(s1Len)(s2Len)
   }
 }
