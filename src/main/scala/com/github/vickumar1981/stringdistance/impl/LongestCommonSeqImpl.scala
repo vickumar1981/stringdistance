f9233b63c29b6a6fa1751499725c6d85754988d0
Vic Kumar <vickumar@gmail.com>
2021-06-08 16:35:18 -0400

    Fix runtime complexity of LongestCommonSeqImpl.scala

--- src/main/scala/com/github/vickumar1981/stringdistance/impl/LongestCommonSeqImpl.scala
+++ src/main/scala/com/github/vickumar1981/stringdistance/impl/LongestCommonSeqImpl.scala
@@ -1,12 +1,19 @@
 package com.github.vickumar1981.stringdistance.impl
 
 private[stringdistance] trait LongestCommonSeqImpl {
-  private def lcs[T](x: Array[T], y: Array[T], m: Int, n: Int): Int = {
-    if (m == 0 || n == 0) 0
-    else if (x(m - 1) == y(n - 1)) 1 + lcs(x, y, m - 1, n - 1)
-    else math.max(lcs(x, y, m, n - 1), lcs(x, y, m - 1, n))
+  protected def longestCommonSeq[T](s1: Array[T], s2: Array[T]): Int = {
+    val m = s1.length
+    val n = s2.length
+    val T = Array.ofDim[Int](m + 1, n + 1)
+    for (i <- 1 to m) {
+      for (j <- 1 to n) {
+        if (s1(i - 1) == s2(j - 1)) {
+          T(i)(j) = T(i - 1)(j - 1) + 1
+        } else {
+          T(i)(j) = math.max(T(i - 1)(j), T(i)(j - 1))
+        }
+      }
+    }
+    T(m)(n)
   }
-
-  protected def longestCommonSeq[T](s1: Array[T], s2: Array[T]): Int =
-    lcs(s1, s2, s1.length, s2.length)
 }
